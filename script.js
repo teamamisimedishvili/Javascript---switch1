1676b3ece8ddfb6978c6b8ce8fbeb4dc7289834f
teamamisimedishvili <127042618+teamamisimedishvili@users.noreply.github.com>
2024-04-28 21:25:20 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const age = 18;
+
+switch (age) {
+    case 10:
+    document.write("haloo");
+    break;
+
+    case 28:
+    document.write("hi");
+    break;
+    
+    case 18:
+        window.alert("სწორია");
+        break;
+
+}
